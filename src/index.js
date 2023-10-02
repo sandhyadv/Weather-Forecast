47d87f3f7957fd017f7de862211bcc3736dc5794
sandhyadv <96787361+sandhyadv@users.noreply.github.com>
2023-10-02 13:22:57 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Routing
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+
+app.get("about/*", (req, res) => {
+  res.render("Error");
+});
+app.get("weather", (req, res) => {
+  res.render("Error");
+});
+app.get("*", (req, res) => {
+  res.render("Error");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://${port}/`);
+});
